84b0bbaca1c859915c6db68b8a4973f9b4fa2ef1
Leonard Chuang <laughing.tiger@hotmail.co.za>
2020-05-27 20:56:44 +0200

    Added insert query for people collection in Leonard.py

--- Leonard.py
+++ Leonard.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 # pprint library is used to make the output look more pretty
 from pprint import pprint
 from random import randint
+import datetime
 
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
 client = MongoClient("mongodb+srv://dbUser:redcloudhorse@cluster0-p0laa.mongodb.net/test?retryWrites=true&w=majority")
@@ -18,11 +19,37 @@ print(db.list_collection_names())
 col = db.People
 
 # Insert documents into the database
-col.insert_one()
+col.insert_one({"threat_level" : 10,
+                "name" : "Jane Doe",
+                "date_of_birth" : datetime.datetime.strptime("2017-10-13T10:53:53.000Z", "%Y-%m-%dT%H:%M:%S.000Z"),
+                "country_of_birth" : "Sudan",
+                "current_location" : {
+                    "address": {
+                        "street": "110 M Market",
+                        "city": "Huffington",
+                        "state": "BB",
+                        "zipcode": "77684"
+                    },
+                    "geo": {
+                        "type": "Point",
+                        "coordinates": [-44.85466 , 93.24565]
+                    },
+                    "timestamp": datetime.datetime.now()
+                },
+                "recent_searches": [
+                    {
+                        "ip": "199.2.04.135",
+                        "device_description": "Samsung S20",
+                        "url": "https://www.mongodb.com/blog/post/6-rules-of-thumb-for-mongodb-schema-design-part-1",
+                        "timestamp": datetime.datetime.now(),
+                        "person_id": "5ecbe3c11c1b2933e5a27033"
+                    }
+                ]
+                })
 
 # Check the documents in the collection
-#for i in col.find():
-    #print(i)
+for i in col.find():
+    print(i)
 
 # Exclude specific fields from the document
 # for x in col.find({},{ "_id": 0 }):

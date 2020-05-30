772dcc30754621e56ae758addc75aec62ab01c93
Keegan WHite <keeganthomaswhite@gmail.com>
2020-05-31 01:43:22 +0200

    Added insert for Locations

--- Keegan.py
+++ Keegan.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson.son import SON
 # pprint library is used to make the output look more pretty
 from pprint import pprint
 from random import randint
@@ -8,6 +9,10 @@ client = MongoClient("mongodb+srv://dbUser:redcloudhorse@cluster0-p0laa.mongodb.
 
 # client.list_database_names()
 # Database name = NSA
-db = client.NSA
+mydb = client.NSA
 # db.list_collection_names()
 # Collections = Files, Locations, People, Searches
+mycol = mydb.Locations
+locationQuery = {"coordinates": {"$near": [-44.8546, 93.24565]}}
+for x in mycol.find(locationQuery):
+    pprint(x)
---
+++ KeeganDataInsert.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+from pprint import pprint
+from random import randint
+from bson import ObjectId
+import datetime
+
+client = MongoClient("mongodb+srv://dbUser:redcloudhorse@cluster0-p0laa.mongodb.net/test?retryWrites=true&w=majority")
+
+db = client.NSA
+col = db.Locations
+
+col.insert_one({
+    "address": {
+        "street": "110 M Market",
+        "city": "Huffington",
+        "state": "BB",
+        "zipcode": "77684"
+    },
+    "geo": {
+        "type": "Point",
+        "coordinates": [-44.85466 , 93.24565]
+    },
+    "timestamp": datetime.datetime.strptime("2019-10-13T10:53:53.000Z", "%Y-%m-%dT%H:%M:%S.000Z"),
+    "person_id": ObjectId("5eceb3f059e783337f050986")
+    })
+
+

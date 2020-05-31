b3dd14c76e906311af91dae102b2a8483c118c47
Keegan WHite <keeganthomaswhite@gmail.com>
2020-05-31 16:51:38 +0200

    Completed 4 Queries

--- Keegan.py
+++ Keegan.py
@@ -12,7 +12,26 @@ client = MongoClient("mongodb+srv://dbUser:redcloudhorse@cluster0-p0laa.mongodb.
 mydb = client.NSA
 # db.list_collection_names()
 # Collections = Files, Locations, People, Searches
-mycol = mydb.Locations
-locationQuery = {"coordinates": {"$near": [-44.8546, 93.24565]}}
-for x in mycol.find(locationQuery):
+locations = mydb.Locations
+searches = mydb.Searches
+people = mydb.People
+
+print("Find all locations that have been recorded that are near [93.24565, -44.8546]")
+locationQuery = {"geo": {"$nearSphere": [93.24565, -44.8546]}}
+for x in locations.find(locationQuery):
     pprint(x)
+
+print("Find all searches done on a Samsung S8")
+phoneQuery = {"device_description": "Samsung S8"}
+for y in searches.find(phoneQuery):
+    pprint(y)
+
+print("Find all people who have names starting with J")
+for i in people.find({"name": {"$gt": "J"}}):
+    pprint(i)
+
+print("Find all entries for peopel who visited https://hangouts.google.com/ or https://www.mongodb.com/blog/post/designing-the-perfect-prototyping-workflow-and-collaboration-platform-in-the-cloud")
+websiteQuery1 = {"url" : "https://hangouts.google.com/"}
+websiteQuery2 = {"url" : "https://www.mongodb.com/blog/post/designing-the-perfect-prototyping-workflow-and-collaboration-platform-in-the-cloud"}
+for j in searches.find({"$or": [websiteQuery1, websiteQuery2]}):
+    pprint(j)
